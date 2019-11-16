ef3c9817e278433c97d81d3ae86c76292701b716
Andrew Shapton <alshapton@gmail.com>
2019-11-16 21:16:10 +0000

    test(tests): added Crew Tests

---
+++ tests/test_crew.nim
@@ -0,0 +1,11 @@
+import unittest 
+import info 
+
+suite "Crew Tests":
+  echo "Testing: crew"
+  
+  test "Crew":
+    let crew_data = crew.crew(2)
+    check(crew_data != "")
+      
+echo "End of suite: Crew Tests"
