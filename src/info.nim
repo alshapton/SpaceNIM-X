03b7ae14ad5b7e0445af9b8bc15000a62fe9bc05
Andrew Shapton <alshapton@gmail.com>
2019-12-16 13:24:53 +0000

    improvement(doc): trial of doc build process

--- src/info.nim
+++ src/info.nim
@@ -1,8 +1,8 @@
-#[
+##[
 Info module
 
 This is a module which allows wrapper access to the technical detail
-of SpaceX, together with information about the API and applications
+of SpaceX, together with information about the API
 using it
 
 This file is imported as a module and contains the following
@@ -11,14 +11,20 @@ functions:
     * company - returns company information
     * api - returns information the API version and endpoints etc
 
-]#
+]##
 
 import urldata
 import utils
 
 
 proc company*(timeOut = 1): string =
+    # Provides information about the SpaceX company
+    #
+    # * Parameter: timeOut         timeout in seconds (default: 1 sec)
     return utils.makeRequest(urldata.main_info, timeOut)
 
 proc api*(timeOut = 1): string =
+    # Provides information about the SpaceX API in use
+    #
+    # * Parameter: timeOut         timeout in seconds (default: 1 sec)
     return utils.makeRequest(urldata.main, timeOut)
